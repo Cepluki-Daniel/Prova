6fad26353da94630cb0560a861fb002e6f9eb1ac
Cepluki-Daniel <47762092+Cepluki-Daniel@users.noreply.github.com>
2020-09-24 00:16:13 -0300

    Add files via upload

---
+++ Cadastro_Alunos/Cadastro_Alunos/Aluno.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastro_Alunos
+{
+    public class Aluno
+    {
+        public string Nome;
+        public string RA;
+        public string CPF;
+        public int Periodo;
+        public string Curso;
+    }
+
+    
+}
